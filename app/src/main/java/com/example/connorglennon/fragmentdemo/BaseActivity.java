b10acc65b10c0445ed4e2d9116dd9f4df96e3b0b
Fat-Llama <connorglennon@outlook.com>
2017-11-21 10:56:25 +0000

    Initial commit

---
+++ app/src/main/java/com/example/connorglennon/fragmentdemo/BaseActivity.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017. Connor Glennon for The App Experts
+ */
+
+package com.example.connorglennon.fragmentdemo;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.connorglennon.fragmentdemo.fragments.FirstFragment;
+import com.example.connorglennon.fragmentdemo.fragments.SecondFragment;
+
+public class BaseActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+
+        fragmentManager = getSupportFragmentManager();
+        button = (Button) findViewById(R.id.btnNews);
+
+        if(savedInstanceState == null)
+        {
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragmentContainer, new FirstFragment()).commit();
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, new SecondFragment()).commit();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/connorglennon/fragmentdemo/fragments/SecondFragment.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017. Connor Glennon for The App Experts
+ */
+
+package com.example.connorglennon.fragmentdemo.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.connorglennon.fragmentdemo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SecondFragment extends Fragment {
+
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
